aed0c66e738ed467b721ff0aef66672f953ef670
zgy-yy <1349604075@qq.com>
2022-05-09 23:39:59 +0800

    播放器界面

---
+++ src/components/PlayerMain/index.tsx
@@ -0,0 +1,13 @@
+import * as React from 'react';
+import { View, Text, Image } from 'remax/wechat';
+import styles from './index.scss';
+
+
+const Index:React.FC= () => {
+  return (
+    <View className={styles.main}>
+    </View>
+  );
+};
+
+export default Index
